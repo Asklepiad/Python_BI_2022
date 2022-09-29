ffc85e5f1fa73806a3ea4aa685f1e53e84ce6b52
Asklepiad <bogdan.sotnikov.1999@mail.ru>
2022-09-29 06:39:12 +0600

    Added binary search

---
+++ Alg_1_Binary_search.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+def binary_search(list, item):
+    low = 0
+    high = len(list)-1
+    while low <= high:
+        mid = int((low + high)/2)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid +1
+    return None    
+
