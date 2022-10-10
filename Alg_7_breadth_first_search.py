d285b46546d58092d6a63594bbb84d5d5f28e247
Asklepiad <bogdan.sotnikov.1999@mail.ru>
2022-10-10 08:36:04 +0600

    Added breadth-first search

---
+++ Alg_7_breadth_first_search.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[34]:
+
+
+from collections import deque
+def person_is_seller(name1):
+    if name1[-1] == 'm': 
+        return True
+    else:
+        return False
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched=[]
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
